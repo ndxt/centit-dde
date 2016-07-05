a812a4de46c8dda32b704297ca19b444023200ee
codefan <codefan@centit.com>
2016-07-05 09:25:45 +0000

    git-svn-id: https://fileserver.centit.com/svn/centit/products/centit-dde3.0@148644 11746a9b-a569-1649-8427-d29ac4cc1c6a

--- src/main/java/com/centit/dde/ws/ValidatorWsImpl.java
+++ src/main/java/com/centit/dde/ws/ValidatorWsImpl.java
@@ -6,13 +6,16 @@ import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.cxf.common.util.CollectionUtils;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 
+import com.centit.dde.po.DatabaseInfo;
 import com.centit.dde.po.UserDataOptId;
 import com.centit.dde.service.UserDataOptIdManager;
 import com.centit.framework.common.SysParametersUtils;
-import com.centit.framework.model.adapter.PlatformEnvironment;
+import com.centit.framework.components.CodeRepositoryUtil;
 import com.centit.framework.model.basedata.IUserInfo;
-import com.centit.support.algorithm.StringBaseOpt;
+import com.centit.framework.model.basedata.IUserUnit;
+import com.centit.support.security.DESSecurityUtils;
 
 /**
  * Created by sx on 2014/12/10.
@@ -21,8 +24,12 @@ public class ValidatorWsImpl implements ValidatorWs {
 
     protected static String wsUnitCode = SysParametersUtils.getStringValue("ws_unit_code");
 
-    protected PlatformEnvironment platformEnvironment;
-
+    private Md5PasswordEncoder passwordEncoder;
+    public void setPasswordEncoder(Md5PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+    
+    
     private UserDataOptIdManager userDataOptIdManager;
 
     public void setUserDataOptIdManager(UserDataOptIdManager userDataOptIdManager) {
@@ -34,27 +41,25 @@ public class ValidatorWsImpl implements ValidatorWs {
         if (StringUtils.isBlank(userName) || StringUtils.isBlank(userPin)) {
             return "参数为空";
         }
-        IUserInfo userInfo = platformEnvironment.getLoginNameRepo().get(userName);
+        IUserInfo userInfo = CodeRepositoryUtil.getUserInfoByLoginName(userName);
         //FUserinfo userInfo = sysUserManager.getUserByLoginname(userName);
         if (null == userInfo) {
             return "1：用户名密码不匹配";
         }
 
-        String password = StringBaseOpt.decryptBase64Des(userPin);
+        String password = DESSecurityUtils.decryptBase64String(userPin,DatabaseInfo.DESKEY);
 
-        if (!userInfo.get.equals(sysUserManager.encodePassword(password, userInfo.getUsercode()))) {
+        if (!userInfo.getUserPin().equals(passwordEncoder.encodePassword(password, userInfo.getUserCode()))) {
             return "1：用户名密码不匹配";
         }
 
         if (!"T".equals(userInfo.getIsValid())) {
             return "1：用户已被禁用";
         }
-
-
-        List<FUserunit> listObjects = sysUnitManager.getSysUnitsByUserId(userInfo.getUsercode());
+        //List<? extends IUserUnit> listObjects =  CodeRepositoryUtil.listUserUnits(userInfo.getUserCode());
         boolean flag = false;
-        for (FUserunit userunit : listObjects) {
-            if (wsUnitCode.equals(userunit.getUnitcode())) {
+        for (IUserUnit userunit : userInfo.getUserUnits()) {
+            if (wsUnitCode.equals(userunit.getUnitCode())) {
                 flag = true;
             }
         }
@@ -69,7 +74,7 @@ public class ValidatorWsImpl implements ValidatorWs {
 
     @Override
     public String validatorDataOptId(String userName, String dataOptId, String dataOptType) {
-        IUserInfo userInfo = platformEnvironment.getLoginNameRepo().get(userName);
+        IUserInfo userInfo = CodeRepositoryUtil.getUserInfoByLoginName(userName);
         Map<String, Object> filterMap = new HashMap<String, Object>();
         filterMap.put("usercode", userInfo.getUserCode());
         filterMap.put("dataOptId", dataOptId);
