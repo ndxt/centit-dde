71d61abaffd4b0c8ff6df258817c152946333f1e
zou_wy <zou_wy@centit.com>
2018-11-13 17:02:15 +0800

    使项目可以启动

--- centit-dde-console/src/main/java/com/centit/dde/config/ServiceConfig.java
+++ centit-dde-console/src/main/java/com/centit/dde/config/ServiceConfig.java
@@ -3,11 +3,13 @@ package com.centit.dde.config;
 
 import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.components.impl.TextOperationLogWriterImpl;
+import com.centit.framework.config.SpringSecurityDaoConfig;
 import com.centit.framework.hibernate.config.HibernateConfig;
-import com.centit.framework.ip.app.config.IPAppSystemBeanConfig;
+import com.centit.framework.ip.app.config.IPOrStaticAppSystemBeanConfig;
+import com.centit.framework.ip.app.service.impl.IntegrationEnvironmentProxy;
+import com.centit.framework.ip.service.IntegrationEnvironment;
 import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
-import com.centit.framework.config.SpringSecurityDaoConfig;
 import org.springframework.context.annotation.*;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -15,10 +17,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableAsync
 @EnableScheduling
-@Import({IPAppSystemBeanConfig.class,
+@Import({IPOrStaticAppSystemBeanConfig.class,
         SpringSecurityDaoConfig.class,
         HibernateConfig.class})
-@ComponentScan(basePackages = {"com.centit.dde"},
+@ComponentScan(basePackages = {"com.centit.dde", "com.centit"},
         excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
 public class ServiceConfig {
 
@@ -41,6 +43,10 @@ public class ServiceConfig {
     @Bean
     public InstantiationServiceBeanPostProcessor instantiationServiceBeanPostProcessor() {
         return new InstantiationServiceBeanPostProcessor();
+    }
+    @Bean
+    public IntegrationEnvironment integrationEnvironment(){
+        return new IntegrationEnvironmentProxy();
     }
 
 }
