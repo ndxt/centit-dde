0182e4a8c1bbbb13cfae8cc917c15c435fc8af42
zfg <zfg@centit.com>
2018-11-26 10:54:48 +0800

    初始化

--- centit-dde-console/src/main/java/com/centit/dde/config/WebInitializer.java
+++ centit-dde-console/src/main/java/com/centit/dde/config/WebInitializer.java
@@ -2,6 +2,7 @@ package com.centit.dde.config;
 
 import com.centit.framework.config.SystemSpringMvcConfig;
 import com.centit.framework.config.WebConfig;
+import com.centit.framework.hibernate.config.HibernateConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -33,6 +34,7 @@ public class WebInitializer implements WebApplicationInitializer {
         WebConfig.registerHiddenHttpMethodFilter(servletContext);
         WebConfig.registerRequestThreadLocalFilter(servletContext);
         WebConfig.registerSpringSecurityFilter(servletContext);
+        HibernateConfig.registerOpenSessionInViewFilter(servletContext);
     }
     /**
      * 加载Spring 配置
