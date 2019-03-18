d2b42bbd6f3a86b6c979c98e52e136a413e909ce
杨淮生 <codefan@centit.com>
2019-03-18 13:32:02 +0800

    迁移到spring boot

--- centit-dde-datamoving/src/main/java/com/centit/dde/datamoving/DataMovingApplication.java
+++ centit-dde-datamoving/src/main/java/com/centit/dde/datamoving/DataMovingApplication.java
@@ -13,8 +13,14 @@ public class DataMovingApplication {
         if(args==null || args.length<1){
             return;
         }
-        String taskId = args[0];
-        System.out.println(taskId);
+        String taskLogId = args[0];
+        System.out.println(taskLogId);
+        //通过 TaskLogId 获取到 TaskId
+        // 通过TaskId 获取 DataPacketId
+        // 通过 DataPacketId创建 DBPacketBizSupplier
+        // 通过TaskInfo 创建 DatabaseBizOperation
+        // 这行 DatabaseBizOperation 就完成工作了
+        // 每一步需要编写日志
         SpringApplication.run(DataMovingApplication.class, args);
     }
 }
