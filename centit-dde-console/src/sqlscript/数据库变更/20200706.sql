5fa815b999c3cf6a78ec109e833eb854b021707d
zhf <zhf@centit.com>
2020-07-20 14:09:29 +0800

    更新脚本修改

--- centit-dde-console/src/sqlscript/数据库变更/20200706.sql
+++ centit-dde-console/src/sqlscript/数据库变更/20200706.sql
@@ -6,6 +6,9 @@ alter table q_data_packet add  column  next_run_time datetime(0);
 alter table q_data_packet add  column  is_valid varchar(1) default 'T';
 alter table q_data_packet add  column  interface_name varchar(64);
 alter table q_data_packet add  column  extend_opt_js text;
+update q_data_packet a, d_exchange_task b
+set a.data_opt_desc_json=b.exchange_desc_json,a.task_type=b.task_type,a.task_cron=b.task_cron,a.is_valid='T'
+where a.PACKET_ID=b.PACKET_ID;
 --postgress
 alter table q_data_packet add  column task_type char(1) NOT NULL DEFAULT '1';
 alter table q_data_packet add  column task_Cron varchar(100);
