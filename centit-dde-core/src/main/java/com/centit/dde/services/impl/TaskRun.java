3eeb61ce4713f61e93e09ae738c80ae2d374874b
zhf <zhf@centit.com>
2020-10-24 19:29:54 +0800

    整理taskrun

--- centit-dde-core/src/main/java/com/centit/dde/services/impl/TaskRun.java
+++ centit-dde-core/src/main/java/com/centit/dde/services/impl/TaskRun.java
@@ -1,30 +1,24 @@
 package com.centit.dde.services.impl;
 
 import com.alibaba.fastjson.JSONObject;
-import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOptFlow;
-import com.centit.dde.core.DataSet;
 import com.centit.dde.dao.DataPacketDao;
 import com.centit.dde.dao.TaskDetailLogDao;
 import com.centit.dde.dao.TaskLogDao;
-import com.centit.dde.dataset.SQLDataSetWriter;
 import com.centit.dde.po.DataPacket;
 import com.centit.dde.po.TaskDetailLog;
 import com.centit.dde.po.TaskLog;
 import com.centit.dde.services.DBPacketBizSupplier;
 import com.centit.fileserver.common.FileStore;
 import com.centit.framework.ip.service.IntegrationEnvironment;
-import com.centit.framework.jdbc.dao.DatabaseOptUtils;
 import com.centit.support.algorithm.CollectionsOpt;
-import com.centit.support.algorithm.NumberBaseOpt;
 import com.centit.support.algorithm.StringBaseOpt;
-import com.centit.support.algorithm.UuidOpt;
 import com.centit.support.common.ObjectException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.support.CronSequenceGenerator;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Date;
 
 /**
  * @author zhf
@@ -35,13 +29,7 @@ public class TaskRun {
     private final TaskDetailLogDao taskDetailLogDao;
     private final DataPacketDao dataPacketDao;
     private final IntegrationEnvironment integrationEnvironment;
-
-    @Autowired(required = false)
-    public void setBizOptFlow(BizOptFlow bizOptFlow) {
-        this.bizOptFlow = bizOptFlow;
-    }
-
-    private BizOptFlow bizOptFlow;
+    private final BizOptFlow bizOptFlow;
     private FileStore fileStore;
 
     @Autowired(required = false)
@@ -54,17 +42,18 @@ public class TaskRun {
     public TaskRun(TaskLogDao taskLogDao,
                    TaskDetailLogDao taskDetailLogDao,
                    DataPacketDao dataPacketDao,
-                   IntegrationEnvironment integrationEnvironment) {
+                   IntegrationEnvironment integrationEnvironment, BizOptFlow bizOptFlow) {
         this.taskLogDao = taskLogDao;
         this.taskDetailLogDao = taskDetailLogDao;
         this.dataPacketDao = dataPacketDao;
         this.integrationEnvironment = integrationEnvironment;
+        this.bizOptFlow = bizOptFlow;
     }
 
-    private BizModel runStep(DataPacket dataPacket, String logId) throws Exception {
+    private void runStep(DataPacket dataPacket, String logId) {
         JSONObject bizOptJson = dataPacket.getDataOptDescJson();
         if (bizOptJson.isEmpty()) {
-            return null;
+            return;
         }
         DBPacketBizSupplier dbPacketBizSupplier = new DBPacketBizSupplier(dataPacket);
         dbPacketBizSupplier.setIntegrationEnvironment(integrationEnvironment);
@@ -72,7 +61,7 @@ public class TaskRun {
         dbPacketBizSupplier.setBatchWise(dataPacket.getIsWhile());
         /*添加参数默认值传输*/
         dbPacketBizSupplier.setQueryParams(dataPacket.getPacketParamsValue());
-        return bizOptFlow.run(dbPacketBizSupplier, bizOptJson, logId);
+        bizOptFlow.run(dbPacketBizSupplier, bizOptJson, logId);
     }
 
 
@@ -88,10 +77,8 @@ public class TaskRun {
     }
 
     public void runTask(String packetId) {
-        BizModel bizModel = null;
         TaskLog taskLog = new TaskLog();
         Date beginTime = new Date();
-        Boolean result;
         try {
             DataPacket dataPacket = dataPacketDao.getObjectWithReferences(packetId);
             dataPacket.setLastRunTime(new Date());
@@ -100,10 +87,11 @@ public class TaskRun {
             taskLog.setRunBeginTime(beginTime);
             taskLog.setRunType(dataPacket.getPacketName());
             taskLogDao.saveNewObject(taskLog);
-            bizModel = runStep(dataPacket, taskLog.getLogId());
+            runStep(dataPacket, taskLog.getLogId());
             taskLog.setRunEndTime(new Date());
             dataPacket.setNextRunTime(new Date());
-            if ("2".equals(dataPacket.getTaskType())
+            String two = "2";
+            if (two.equals(dataPacket.getTaskType())
                 && dataPacket.getIsValid()
                 && !StringBaseOpt.isNvl(dataPacket.getTaskCron())) {
                 CronSequenceGenerator cronSequenceGenerator = new CronSequenceGenerator(dataPacket.getTaskCron());
@@ -111,8 +99,8 @@ public class TaskRun {
             }
             dataPacketDao.updateObject(dataPacket);
             TaskDetailLog taskDetailLog = taskDetailLogDao.getObjectByProperties(
-                CollectionsOpt.createHashMap("logId",taskLog.getLogId(),"logInfo_ne","ok"));
-            taskLog.setOtherMessage(taskDetailLog==null ? "ok" : "error");
+                CollectionsOpt.createHashMap("logId", taskLog.getLogId(), "logInfo_ne", "ok"));
+            taskLog.setOtherMessage(taskDetailLog == null ? "ok" : "error");
             taskLogDao.updateObject(taskLog);
         } catch (Exception e) {
             saveDetail(ObjectException.extortExceptionMessage(e, 4),
