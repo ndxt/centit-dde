24dbf010b36c83e9ea12a79e46d51d9eb1c4db69
zhou_c <zhou_c@centit.com>
2021-05-14 13:42:25 +0800

    修改赋值标签（新增往某个数据集中添加某个值）

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/AssignmentBizOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/AssignmentBizOperation.java
@@ -1,12 +1,13 @@
 package com.centit.dde.bizopt;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataSet;
 import com.centit.dde.core.SimpleDataSet;
 import com.centit.dde.utils.BizModelJSONTransform;
-import com.centit.dde.utils.DataSetOptUtil;
 import com.centit.dde.vo.AssignmentVo;
 import com.centit.framework.common.ResponseData;
 import com.centit.support.json.JSONTransformer;
@@ -15,7 +16,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -35,10 +35,25 @@ public class AssignmentBizOperation implements BizOperation {
         DataSet newDataSet;//默认引用数据集
         if ("1".equals(assignmentVo.getAssignType())){//复制
             newDataSet=SerializationUtils.clone(new SimpleDataSet(dataSet.getData()));
-        }else if ("3".equals(assignmentVo.getAssignType())){//手动赋值
-            newDataSet=new SimpleDataSet(assignmentVo.getData());
-        }else {
-            newDataSet=dataSet;
+        } else if ("2".equals(assignmentVo.getAssignType())) {//合并  （将一个字段的值添加到数据集中）
+            Object data = JSONTransformer.transformer(assignmentVo.getExpression(), new BizModelJSONTransform(bizModel));
+            List<Map<String, Object>> dataAsList = dataSet.getDataAsList();
+            List<Map<String, Object>> tempList = new ArrayList<>();
+            JSONArray jsonArray = JSON.parseArray(JSON.toJSONString(data));
+            for (Object object : jsonArray) {
+                Map map = JSONObject.parseObject(JSON.toJSONString(object), Map.class);
+                tempList.add(map);
+            }
+            for (Map<String, Object> objectMap : dataAsList) {
+                for (Map<String, Object> stringObjectMap : tempList) {
+                    objectMap.putAll(stringObjectMap);
+                }
+            }
+            newDataSet=new SimpleDataSet(dataAsList);
+        } else if ("3".equals(assignmentVo.getAssignType())) {//手动赋值
+            newDataSet = new SimpleDataSet(assignmentVo.getData());
+        } else {
+            newDataSet = dataSet;
         }
         if (StringUtils.isNotBlank(assignmentVo.getExpression())){
             Object data = JSONTransformer.transformer(assignmentVo.getExpression(), new BizModelJSONTransform(bizModel));
