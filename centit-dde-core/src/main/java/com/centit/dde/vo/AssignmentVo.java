b859ad96691f6b6409d5b5987b136d626f151e4c
zhou_c <zhou_c@centit.com>
2021-05-14 09:07:05 +0800

    恢复以前实现方式

---
+++ centit-dde-core/src/main/java/com/centit/dde/vo/AssignmentVo.java
@@ -0,0 +1,25 @@
+package com.centit.dde.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 赋值节点
+ */
+@ApiModel
+@Data
+public class AssignmentVo {
+    @ApiModelProperty("节点id")
+    private String id;
+    @ApiModelProperty(value = "节点类型")
+    private String type;
+    @ApiModelProperty(value = "赋值类型：复制或者引用  1:复制   2：引用，3：手动赋值")
+    private String assignType="2";
+    @ApiModelProperty(value = "赋值的数据（简单数值或者json串）")
+    private Object data;
+    @ApiModelProperty(value = "数据节点id")
+    private String source;
+    @ApiModelProperty(value = "表达式")
+    private String expression;
+}
