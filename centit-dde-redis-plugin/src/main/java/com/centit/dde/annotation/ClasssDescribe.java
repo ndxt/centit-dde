b89ca8c29f8cd5cfc366f86f20873f82b00cc9bb
zhou_c <zhou_c@centit.com>
2021-06-11 10:00:01 +0800

    添加redis标签实现，部分功能待测

--- centit-dde-redis-plugin/src/main/java/com/centit/dde/annotation/ClasssDescribe.java
+++ centit-dde-redis-plugin/src/main/java/com/centit/dde/annotation/ClasssDescribe.java
@@ -4,7 +4,6 @@ import java.lang.annotation.*;
 
 /**
  * @author zhou_c
- * 用于扫描类的方方法和方法使用说明
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
--- centit-dde-redis-plugin/src/main/java/com/centit/dde/annotation/MethodDescribe.java
+++ centit-dde-redis-plugin/src/main/java/com/centit/dde/annotation/MethodDescribe.java
@@ -4,7 +4,7 @@ import java.lang.annotation.*;
 
 /**
  * @author zhou_c
- * 用于扫描类的方方法和方法使用说明
+ * 用于方法的使用说明和参数说明
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
