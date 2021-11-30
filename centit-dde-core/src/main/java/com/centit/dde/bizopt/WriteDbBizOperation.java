1aff5f566fa2f2b09be8134d3af945d8694d7a97
zhou_c <zhou_c@centit.com>
2021-11-30 14:45:40 +0800

    修改元数据标签实现

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/WriteDbBizOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/WriteDbBizOperation.java
@@ -1,5 +1,6 @@
 package com.centit.dde.bizopt;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
@@ -7,11 +8,9 @@ import com.centit.dde.core.DataSet;
 import com.centit.dde.core.SimpleDataSet;
 import com.centit.framework.common.ResponseData;
 import com.centit.product.metadata.service.MetaObjectService;
+import com.centit.support.database.utils.PageDesc;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class WriteDbBizOperation implements BizOperation {
     MetaObjectService metaObjectService;
@@ -29,21 +28,63 @@ public class WriteDbBizOperation implements BizOperation {
         String source = bizOptJson.getString("source");
         String tableId = bizOptJson.getString("tableLabelName");
         Integer withChildrenDeep = bizOptJson.getInteger("withChildrenDeep");
+        Map<String, Object> modelTag = bizModel.getModelTag();
+        modelTag.remove("runType");
         DataSet dataSet = bizModel.getDataSet(source);
-        if (dataSet == null) {
-            return BuiltInOperation.getResponseData(0, 500, bizOptJson.getString("SetsName") + "：未指定数据集,或指定的数据集为NULL！");
+        if (dataSet==null){
+            SimpleDataSet simpleDataSet = new SimpleDataSet();
+            simpleDataSet.setData(modelTag);
+            dataSet = simpleDataSet;
         }
+        JSONObject properties = bizOptJson.getJSONObject("properties");
+        Integer templateType = properties.getInteger("templateType");
+        List<Map<String, Object>> dataAsList = dataSet.getDataAsList();
         try {
-            List<Map<String, Object>> dataAsList = dataSet.getDataAsList();
-            int count = 0;
-            for (Map<String, Object> objectMap : dataAsList) {
-                count += metaObjectService.mergeObjectWithChildren(tableId, objectMap, withChildrenDeep == null ? 1 : withChildrenDeep);
+            switch (templateType){
+                case 1://新建
+                    int saveCount = 0;
+                    for (Map<String, Object> objectMap : dataAsList) {
+                        saveCount += metaObjectService.saveObjectWithChildren(tableId, objectMap, withChildrenDeep == null ? 1 : withChildrenDeep);
+                    }
+                    bizModel.putDataSet(id, new SimpleDataSet(saveCount));
+                    return BuiltInOperation.getResponseSuccessData(saveCount);
+                case 2://修改
+                    int updateCount = 0;
+                    for (Map<String, Object> objectMap : dataAsList) {
+                        updateCount += metaObjectService.updateObjectWithChildren(tableId, objectMap, withChildrenDeep == null ? 1 : withChildrenDeep);
+                    }
+                    bizModel.putDataSet(id, new SimpleDataSet(updateCount));
+                    return BuiltInOperation.getResponseSuccessData(updateCount);
+                case 3://删除
+                    int deleteCount = 0;
+                    for (Map<String, Object> objectMap : dataAsList) {
+                        metaObjectService.deleteObjectWithChildren(tableId, objectMap, withChildrenDeep == null ? 1 : withChildrenDeep);
+                        deleteCount++;
+                    }
+                    bizModel.putDataSet(id, new SimpleDataSet(deleteCount));
+                    return BuiltInOperation.getResponseSuccessData(deleteCount);
+                case 4://查询
+                    JSONArray jsonArray =new JSONArray();
+                    for (Map<String, Object> objectMap : dataAsList) {
+                        PageDesc pageDesc = new PageDesc();
+                        pageDesc.setPageNo(Integer.valueOf(String.valueOf(objectMap.get("pageNo"))));
+                        pageDesc.setPageSize(Integer.valueOf(String.valueOf(objectMap.get("pageSize"))));
+                        jsonArray = metaObjectService.pageQueryObjects(tableId, objectMap,pageDesc);
+                    }
+                    bizModel.putDataSet(id, new SimpleDataSet(jsonArray));
+                    return BuiltInOperation.getResponseSuccessData(jsonArray.size());
+                case 5://查看
+                    Map<String, Object> objectById = new HashMap<>();
+                    for (Map<String, Object> objectMap : dataAsList) {
+                        objectById= metaObjectService.getObjectById(tableId, objectMap);
+                    }
+                    bizModel.putDataSet(id, new SimpleDataSet(objectById));
+                    return BuiltInOperation.getResponseSuccessData(objectById.size());
             }
-            bizModel.putDataSet(id, new SimpleDataSet(count));
-            return BuiltInOperation.getResponseSuccessData(count);
         } catch (Exception e) {
             bizModel.putDataSet(id,new SimpleDataSet(e.getMessage()));
             return BuiltInOperation.getResponseData(0, 1, e.getMessage());
         }
+        return null;
     }
 }
