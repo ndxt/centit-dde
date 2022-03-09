4c9050ebf8d2a4d8cde8c3332c815072cf08df9d
zhou_c <zhou_c@centit.com>
2022-03-09 09:48:00 +0800

    修改日志

--- centit-dde-agent/src/main/java/com/centit/dde/agent/service/RunTaskJob.java
+++ centit-dde-agent/src/main/java/com/centit/dde/agent/service/RunTaskJob.java
@@ -47,7 +47,7 @@ public class RunTaskJob extends AbstractQuartzJob {
             }
             try {
                 TaskRun taskRun = ContextUtils.getBean(TaskRun.class);
-                logger.info(String.format("开始执行定时任务:%s,任务ID：%s,cron表达式：%s",dataPacket.getPacketName(),dataPacket.getPacketId(),dataPacket.getTaskCron()));
+                logger.info(String.format("execute scheduled tasks,taskName:%s,taskID：%s,cron expression：%s",dataPacket.getPacketName(),dataPacket.getPacketId(),dataPacket.getTaskCron()));
                 taskRun.runTask(dataPacket.getPacketId(), null,new HashMap<>());
             } finally {
                 runningTask.put(dataPacket.getPacketId(), false);
