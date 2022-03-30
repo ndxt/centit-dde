08c0e938152f5581b170ebf165b138b6a4c56482
zhou_c <zhou_c@centit.com>
2022-03-30 10:20:51 +0800

    修改生成Excel实现

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/ExcelBizOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/ExcelBizOperation.java
@@ -12,7 +12,6 @@ import com.centit.framework.common.ResponseData;
 import com.centit.support.json.JSONTransformer;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,15 +28,13 @@ public class ExcelBizOperation implements BizOperation {
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson) throws Exception {
         String id =bizOptJson.getString("id");
         String source = bizOptJson.getString("source");
-        //文件流  还是  字段（保存文件的字段）
-        //String sourceType = bizOptJson.getString("sourceType");
         Map<String, Object> params = new HashMap<>();
-        String sheetName = bizOptJson.getString("sheetName")==null?"": bizOptJson.getString("sheetName");
-        String headerRow = bizOptJson.getString("headerRow")==null?"0":bizOptJson.getString("headerRow");
-        String beginRow = bizOptJson.getString("beginRow")==null?"0":bizOptJson.getString("beginRow");
-        String endRow = bizOptJson.getString("endRow")==null?"0":bizOptJson.getString("endRow");
-        String startColumnNumber = bizOptJson.getString("startColumnNumber")==null?"0":bizOptJson.getString("startColumnNumber");
-        String endColumnNumber = bizOptJson.getInteger("endColumnNumber")==null?"0":bizOptJson.getString("endColumnNumber");
+        String sheetName = StringUtils.isBlank(bizOptJson.getString("sheetName"))?"": bizOptJson.getString("sheetName");
+        String headerRow =  StringUtils.isBlank(bizOptJson.getString("headerRow"))?"0":bizOptJson.getString("headerRow");
+        String beginRow =  StringUtils.isBlank(bizOptJson.getString("beginRow"))?"0":bizOptJson.getString("beginRow");
+        String endRow =  StringUtils.isBlank(bizOptJson.getString("endRow"))?"0":bizOptJson.getString("endRow");
+        String startColumnNumber =  StringUtils.isBlank(bizOptJson.getString("startColumnNumber"))?"0":bizOptJson.getString("startColumnNumber");
+        String endColumnNumber =  StringUtils.isBlank(bizOptJson.getString("endColumnNumber"))?"0":bizOptJson.getString("endColumnNumber");
         params.put("sheetName",JSONTransformer.transformer(sheetName, new BizModelJSONTransform(bizModel)));
         params.put("headerRow",JSONTransformer.transformer(headerRow, new BizModelJSONTransform(bizModel)));
         params.put("beginRow",JSONTransformer.transformer(beginRow, new BizModelJSONTransform(bizModel)));
