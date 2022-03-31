468a7d30366dd979ec15d6417a2b817aecee3b1c
zhou_c <zhou_c@centit.com>
2022-03-31 15:32:08 +0800

    添加交集，差集和元数据合并功能

---
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/IntersectDataSetBizOperation.java
@@ -0,0 +1,29 @@
+package com.centit.dde.bizopt;
+
+import com.alibaba.fastjson.JSONObject;
+import com.centit.dde.core.BizModel;
+import com.centit.dde.core.BizOperation;
+import com.centit.dde.core.DataSet;
+import com.centit.dde.utils.DataSetOptUtil;
+import com.centit.framework.common.ResponseData;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ *求2个数据的交集
+ */
+public class IntersectDataSetBizOperation implements BizOperation {
+
+    @Override
+    public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson) throws Exception {
+        String id = bizOptJson.getString("id");
+        String mainDataSet = bizOptJson.getString("mainDataSet");
+        String slaveDataSet = bizOptJson.getString("slaveDataSet");
+        boolean unionData =bizOptJson.getBoolean("unionData");
+        Map<String, String> pks = BuiltInOperation.jsonArrayToMap(bizOptJson.getJSONArray("primaryFields"), "mainField", "slaveField");
+        DataSet dataSet = DataSetOptUtil.intersectTwoDataSet(bizModel.getDataSet(mainDataSet), bizModel.getDataSet(slaveDataSet), new ArrayList<>(pks.entrySet()), unionData);
+        bizModel.putDataSet(id,dataSet);
+        return BuiltInOperation.getResponseSuccessData(dataSet==null?0:dataSet.getSize());
+    }
+}
---
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/MinusDataSetBizOperation.java
@@ -0,0 +1,28 @@
+package com.centit.dde.bizopt;
+
+import com.alibaba.fastjson.JSONObject;
+import com.centit.dde.core.BizModel;
+import com.centit.dde.core.BizOperation;
+import com.centit.dde.core.DataSet;
+import com.centit.dde.utils.DataSetOptUtil;
+import com.centit.framework.common.ResponseData;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * 差集
+ */
+public class MinusDataSetBizOperation implements BizOperation {
+
+    @Override
+    public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson) throws Exception {
+        String id = bizOptJson.getString("id");
+        String mainDataSet = bizOptJson.getString("mainDataSet");
+        String slaveDataSet = bizOptJson.getString("slaveDataSet");
+        Map<String, String> pks = BuiltInOperation.jsonArrayToMap(bizOptJson.getJSONArray("primaryFields"), "mainField", "slaveField");
+        DataSet dataSet = DataSetOptUtil.minusTwoDataSet(bizModel.getDataSet(mainDataSet), bizModel.getDataSet(slaveDataSet), new ArrayList<>(pks.entrySet()));
+        bizModel.putDataSet(id,dataSet);
+        return BuiltInOperation.getResponseSuccessData(dataSet==null?0:dataSet.getSize());
+    }
+}
