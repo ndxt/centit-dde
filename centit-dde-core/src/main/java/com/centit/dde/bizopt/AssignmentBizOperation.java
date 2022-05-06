ed4087cf3621959d65270381223dcc2475aebaca
codefan <codefan@sina.com>
2022-05-06 10:53:31 +0800

    添加赋值组件

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/AssignmentBizOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/AssignmentBizOperation.java
@@ -1,5 +1,6 @@
 package com.centit.dde.bizopt;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
@@ -7,6 +8,13 @@ import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
 import com.centit.dde.utils.DatasetVariableTranslate;
 import com.centit.framework.common.ResponseData;
+import com.centit.support.algorithm.CollectionsOpt;
+import com.centit.support.json.JSONOpt;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class AssignmentBizOperation implements BizOperation {
 
@@ -22,22 +30,50 @@ public class AssignmentBizOperation implements BizOperation {
         //取值表达式
         String formula = BuiltInOperation.getJsonFieldString(bizOptJson, "formula", ".");
         DataSet dataSet =bizModel.fetchDataSetByName(sourceDsName);
-        Object sourceData = new DatasetVariableTranslate(dataSet).getVarValue(formula);
 
         DataSet targetDataSet =bizModel.fetchDataSetByName(targetDsName);
-        if("property".equals(assignType)){
+        if(dataSet == null  || targetDataSet==null){
+            return BuiltInOperation.createResponseData(0, ResponseData.ERROR_USER_CONFIG,
+                "配置信息出错，找不到对应的数据集："+targetDsName+"，"+sourceDsName+"。");
+        }
+        Object sourceData = new DatasetVariableTranslate(dataSet).getVarValue(formula);
+        String property = null;
+        if("property".equals(assignType)) {
             //复制属性
-            String property = BuiltInOperation.getJsonFieldString(bizOptJson, "property", ".");
+            property = BuiltInOperation.getJsonFieldString(bizOptJson, "property", ".");
+        }
+        // 对属性赋值
+        if("property".equals(assignType) && StringUtils.isNotBlank(property) && !".".equals(property)){
             // property
             if( targetDataSet.getSize() < 1){
-
-            } else if( targetDataSet.getSize() == 1){
-
+                JSONObject jsonObject = new JSONObject();
+                JSONOpt.appendData(jsonObject, property, sourceData);
+                targetDataSet.setData(jsonObject);
+            } else if(targetDataSet.getSize() == 1){
+                Map<String, Object> objMap = targetDataSet.getFirstRow();
+                JSONObject jsonObject = (JSONObject)JSON.toJSON(objMap);
+                JSONOpt.appendData(jsonObject, property, sourceData);
+                targetDataSet.setData(jsonObject);
             } else {
-
+                List<Map<String, Object>> objList = targetDataSet.getDataAsList();
+                List<JSONObject> jsonList = new ArrayList<>(objList.size() +1);
+                for(Map<String, Object> objMap : objList){
+                    JSONObject jsonObject = (JSONObject)JSON.toJSON(objMap);
+                    JSONOpt.appendData(jsonObject, property, sourceData);
+                    jsonList.add(jsonObject);
+                }
+                targetDataSet.setData(jsonList);
             }
-        } else {
-
+        } else if("append".equals(assignType)){ // 追加
+            if( targetDataSet.getSize() < 1){
+                targetDataSet.setData(CollectionsOpt.createList(sourceData));
+            } else {
+                List<Map<String, Object>> objList = targetDataSet.getDataAsList();
+                objList.add(CollectionsOpt.objectToMap(sourceData));
+                targetDataSet.setData(objList);
+            }
+        } else { // 对目标对象赋值 assign || property == '.'
+            targetDataSet.setData(sourceData);
         }
         return BuiltInOperation.createResponseSuccessData(targetDataSet.getSize());
     }
