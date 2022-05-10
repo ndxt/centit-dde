cada0b5dc0244790bf0ef259cae8a0b75e4b529e
zhf <zhf@centit.com>
2022-05-10 14:31:35 +0800

    修改

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/MetadataUpdateBizOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/MetadataUpdateBizOperation.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataOptContext;
+import com.centit.dde.core.DataSet;
 import com.centit.dde.utils.DataSetOptUtil;
 import com.centit.framework.common.ResponseData;
 import com.centit.product.metadata.service.MetaObjectService;
@@ -32,6 +33,7 @@ public class MetadataUpdateBizOperation implements BizOperation {
         switch (updateType){
             case 1://新建
                 int count = metaObjectService.saveObjectWithChildren(tableId, parames, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(parames));
                 return BuiltInOperation.createResponseSuccessData(count);
             case 2://修改
                 int upcount= metaObjectService.updateObjectWithChildren(tableId, parames, withChildrenDeep);
@@ -41,6 +43,7 @@ public class MetadataUpdateBizOperation implements BizOperation {
                 return BuiltInOperation.createResponseSuccessData(1);
             case 4://合并
                 int resultCount = metaObjectService.mergeObjectWithChildren(tableId, parames, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(parames));
                 return BuiltInOperation.createResponseSuccessData(resultCount);
             default:
                 return ResponseData.makeErrorMessage("未知操作类型！");
