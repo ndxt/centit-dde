f2438c30e510a50d96161974378dd3a432adb740
周抄 <zhou_c@centit.com>
2022-09-22 15:46:39 +0800

    添加es文档，文件 增 删  改  查 实现

--- centit-dde-es-plugin/src/main/java/com/centit/dde/EsQueryBizOperation.java
+++ centit-dde-es-plugin/src/main/java/com/centit/dde/EsQueryBizOperation.java
@@ -8,23 +8,30 @@ import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
 import com.centit.dde.utils.BizModelJSONTransform;
 import com.centit.framework.common.ResponseData;
+import com.centit.framework.core.dao.PageQueryResult;
 import com.centit.search.document.FileDocument;
 import com.centit.search.document.ObjectDocument;
 import com.centit.search.service.ESServerConfig;
 import com.centit.search.service.Impl.ESSearcher;
 import com.centit.search.service.IndexerSearcherFactory;
+import com.centit.support.algorithm.CollectionsOpt;
 import com.centit.support.algorithm.NumberBaseOpt;
 import com.centit.support.algorithm.StringBaseOpt;
-import com.centit.support.json.JSONTransformer;
+import com.centit.support.database.utils.PageDesc;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.MultiMatchQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EsQueryBizOperation implements BizOperation {
-    private ESServerConfig esServerConfig;
+    private final ESServerConfig esServerConfig;
 
     public EsQueryBizOperation(ESServerConfig esServerConfig) {
         this.esServerConfig = esServerConfig;
@@ -32,48 +39,76 @@ public class EsQueryBizOperation implements BizOperation {
 
     @Override
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext){
-        String queryParameter = bizOptJson.getString("queryParameter");
-        if(StringBaseOpt.isNvl(queryParameter)) return ResponseData.makeErrorMessage("查询关键字不能为空！");
-
-        String osId = dataOptContext.getOsId();
-        if (StringUtils.isBlank(osId)) return ResponseData.makeErrorMessage("osId不能为空！");
-
-        BizModelJSONTransform bizModelJSONTransform = new BizModelJSONTransform(bizModel);
         Map<String,Object> queryParam = new HashMap<>(6);
-        queryParam.put("osId",osId);
-
+        queryParam.put("osId",dataOptContext.getOsId());
+        BizModelJSONTransform transform = new BizModelJSONTransform(bizModel);
         if (!bizOptJson.getBoolean("queryAll")){
-            String optTag = bizOptJson.getString("optTag");
-            String unitCode = bizOptJson.getString("unitCode");
-            String userCode = bizOptJson.getString("userCode");
-            if (StringUtils.isNotBlank(optTag)){
-                queryParam.put("optTag",
-                    StringBaseOpt.castObjectToString(JSONTransformer.transformer(optTag, bizModelJSONTransform)));
-            }
-            if (StringUtils.isNotBlank(unitCode)){
-                queryParam.put("unitCode",
-                    StringBaseOpt.castObjectToString(JSONTransformer.transformer(unitCode, bizModelJSONTransform)));
-            }
-            if (StringUtils.isNotBlank(userCode)){
-                queryParam.put("userCode",
-                    StringBaseOpt.castObjectToString(JSONTransformer.transformer(userCode, bizModelJSONTransform)));
-            }
+            Object optTag = transform.attainExpressionValue(bizOptJson.getString("optTag"));
+            Object unitCode = transform.attainExpressionValue(bizOptJson.getString("unitCode"));
+            Object userCode = transform.attainExpressionValue(bizOptJson.getString("userCode"));
+            if (optTag != null ) queryParam.put("optTag", optTag);
+            if (unitCode != null ) queryParam.put("unitCode", unitCode);
+            if (userCode != null ) queryParam.put("userCode",userCode);
         }
 
-        String pageSize = bizOptJson.getString("pageSize");
-        String pageNo = bizOptJson.getString("pageNo");
-        Integer pageSizeTrans = NumberBaseOpt.castObjectToInteger(JSONTransformer.transformer(pageSize, bizModelJSONTransform),1);
-        Integer pageNoTrans = NumberBaseOpt.castObjectToInteger(JSONTransformer.transformer(pageNo, bizModelJSONTransform),20);
+        int pageNo  = NumberBaseOpt.castObjectToInteger(transform.attainExpressionValue(bizOptJson.getString("pageNo")),1);
+        int pageSize = NumberBaseOpt.castObjectToInteger(transform.attainExpressionValue(bizOptJson.getString("pageSize")),20);
 
         boolean indexFile = "indexFile".equals(bizOptJson.get("indexType"));
         ESSearcher esSearcher = indexFile ? IndexerSearcherFactory.obtainSearcher(esServerConfig, FileDocument.class)
             :IndexerSearcherFactory.obtainSearcher(esServerConfig, ObjectDocument.class);
 
-        String keyword = StringBaseOpt.castObjectToString(JSONTransformer.transformer(queryParameter, bizModelJSONTransform));
-
-        Pair<Long, List<Map<String, Object>>> search = esSearcher.search(queryParam, keyword, pageNoTrans, pageSizeTrans);
-        DataSet dataSet = new DataSet(search.getValue());
+        String keyword = StringBaseOpt.castObjectToString(transform.attainExpressionValue(bizOptJson.getString("queryParameter")));
+        if(StringUtils.isBlank(keyword)) {
+            return ResponseData.makeErrorMessage("查询关键字不能为空！");
+        }
+        //Pair<Long, List<Map<String, Object>>> search = esSearcher.search(queryParam, keyword, pageNo,pageSize);
+        QueryBuilder queryBuilder = queryBuilder(queryParam, keyword, indexFile, esSearcher, bizOptJson, transform);
+        Pair<Long, List<Map<String, Object>>> search = esSearcher.esSearch(queryBuilder,pageNo,pageSize);
+        PageDesc pageDesc = new PageDesc();
+        pageDesc.setPageNo(pageNo);
+        pageDesc.setPageSize(pageSize);
+        pageDesc.setTotalRows(NumberBaseOpt.castObjectToInteger(search.getLeft()));
+        PageQueryResult<Map<String, Object>> result = PageQueryResult.createResult(search.getRight(), pageDesc);
+        DataSet dataSet = new DataSet(result);
         bizModel.putDataSet(bizOptJson.getString("id"),dataSet);
         return BuiltInOperation.createResponseSuccessData(dataSet.getSize());
+    }
+
+    private QueryBuilder queryBuilder(Map<String,Object> queryParam, String keyword,Boolean indexFile,
+                                      ESSearcher esSearcher,JSONObject bizOptJson,BizModelJSONTransform transform){
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        if (queryParam != null){
+            for (Map.Entry<String, Object> entry : queryParam.entrySet()) {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                if (StringUtils.isBlank(key) || value == null) continue;
+                if (value.getClass().isArray()) {
+                    boolQueryBuilder.must(QueryBuilders.termsQuery(key,(String[])value));
+                } else if (value instanceof Collection) {
+                    boolQueryBuilder.must(QueryBuilders.termsQuery(key,CollectionsOpt.listToArray((Collection)value)));
+                } else {
+                    boolQueryBuilder.must(QueryBuilders.termQuery(key, value));
+                }
+            }
+        }
+        esSearcher.initTypeFields(indexFile?FileDocument.class:ObjectDocument.class);
+        if (StringUtils.isNotBlank(keyword)){
+            MultiMatchQueryBuilder multiMatchQueryBuilder = QueryBuilders.multiMatchQuery(keyword, esSearcher.getQueryFields());
+            Boolean custom = bizOptJson.getBoolean("custom");
+            if (custom){
+                String analyzer = StringBaseOpt.castObjectToString(transform.attainExpressionValue(bizOptJson.getString("analyzer")));
+                String minimumShouldMatch = StringBaseOpt.castObjectToString(
+                    transform.attainExpressionValue(bizOptJson.getString("minimumShouldMatch")));
+                if (StringUtils.isNotBlank(analyzer)){
+                    multiMatchQueryBuilder.analyzer(analyzer);
+                }
+                if (StringUtils.isNotBlank(minimumShouldMatch)){
+                    multiMatchQueryBuilder.minimumShouldMatch(minimumShouldMatch);
+                }
+            }
+            boolQueryBuilder.must(multiMatchQueryBuilder);
+        }
+        return boolQueryBuilder;
     }
 }
--- centit-dde-es-plugin/src/main/java/com/centit/dde/EsWriteBizOperation.java
+++ centit-dde-es-plugin/src/main/java/com/centit/dde/EsWriteBizOperation.java
@@ -18,15 +18,17 @@ import com.centit.search.service.IndexerSearcherFactory;
 import com.centit.search.utils.TikaTextExtractor;
 import com.centit.support.algorithm.StringBaseOpt;
 import com.centit.support.compiler.VariableFormula;
+import com.centit.support.json.JSONTransformer;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 public class EsWriteBizOperation implements BizOperation {
 
-    private ESServerConfig esServerConfig;
+    private final ESServerConfig esServerConfig;
 
     public EsWriteBizOperation(ESServerConfig esServerConfig) {
         this.esServerConfig = esServerConfig;
@@ -35,93 +37,131 @@ public class EsWriteBizOperation implements BizOperation {
 
     @Override
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext) throws Exception {
-
-        String dataSetId = bizOptJson.getString("source");
-        if (StringBaseOpt.isNvl(dataSetId)) {
-            return ResponseData.makeErrorMessage("请选择需要写入的数据！");
-        }
-
-        DataSet dataSet = bizModel.getDataSet(dataSetId);
-        if (dataSet == null ){
-            return ResponseData.makeErrorMessage("选择的数据集不存在！");
+        //操作类型  增  删  改  合并
+        String operationType = bizOptJson.getString("operationType");
+        if (StringUtils.isBlank(operationType)){
+            return ResponseData.makeErrorMessage("请选择操作类型！");
         }
 
         boolean indexFile = "indexFile".equals(bizOptJson.get("indexType"));
-        ESIndexer esIndexer = indexFile ? IndexerSearcherFactory.obtainIndexer(esServerConfig, FileDocument.class)
+        ESIndexer esIndexer = indexFile ?
+            IndexerSearcherFactory.obtainIndexer(esServerConfig, FileDocument.class)
             :IndexerSearcherFactory.obtainIndexer(esServerConfig, ObjectDocument.class);
 
-        String optTag = bizOptJson.getString("optTag");
-        if (StringUtils.isBlank(optTag)){
-            return ResponseData.makeErrorMessage("业务主键不能为空！");
+        return indexFile?
+            fileDocumentOperation(bizModel,bizOptJson,dataOptContext,esIndexer,operationType)
+            : objectDocumentOperation(bizModel,bizOptJson,dataOptContext,esIndexer,operationType);
+    }
+    //es 文件文档操作
+    private ResponseData fileDocumentOperation(BizModel bizModel, JSONObject bizOptJson,DataOptContext dataOptContext,
+                                               ESIndexer esIndexer,String operationType) throws Exception{
+        BizModelJSONTransform transform = new BizModelJSONTransform(bizModel);
+        Object documentId = transform.attainExpressionValue(bizOptJson.getString("documentId"));
+        if (documentId == null) return ResponseData.makeErrorMessage("文档主键不能为空！");
+
+        if (!"delete".equals(operationType)){
+            String dataSetId = bizOptJson.getString("source");
+            if (StringUtils.isBlank(dataSetId)) return ResponseData.makeErrorMessage("文档内容不能为空！");
+
+            DataSet dataSet = bizModel.getDataSet(dataSetId);
+            if (dataSet == null ) return ResponseData.makeErrorMessage("选择的文档内容不存在！");
+
+            Object optTag = transform.attainExpressionValue(bizOptJson.getString("optTag"));
+            if (optTag == null) return ResponseData.makeErrorMessage("业务主键不能为空！");
         }
-        if(indexFile){
-            FileDocument fileInfo = new FileDocument();
-            fileInfo.setOptId(dataOptContext.getOptId());
-            fileInfo.setOsId(dataOptContext.getOsId());
+        Object result;
+        switch (operationType){
+            case "add":
+                result = esIndexer.saveNewDocument(fileDocumentBuild(bizModel, bizOptJson, dataOptContext,transform));
+                break;
+            case "delete":
+                result= esIndexer.deleteDocument(StringBaseOpt.castObjectToString(documentId));
+                break;
+            case "update":
+                result = esIndexer.updateDocument(fileDocumentBuild(bizModel, bizOptJson, dataOptContext,transform));
+                break;
+            default: //合并
+                result = esIndexer.mergeDocument(fileDocumentBuild(bizModel, bizOptJson, dataOptContext,transform));
+                break;
+        }
+        return ResponseData.makeResponseData(result);
+    }
+    //构建文件文档信息
+    private FileDocument fileDocumentBuild(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext,BizModelJSONTransform modelTrasform) throws Exception{
+        DataSet dataSet = bizModel.getDataSet(bizOptJson.getString("source"));
+        FileDocument fileInfo = new FileDocument();
+        fileInfo.setOptId(dataOptContext.getOptId());
+        fileInfo.setOsId(dataOptContext.getOsId());
+        fileInfo.setOptMethod("index");
+        fileInfo.setUserCode(dataOptContext.getCurrentUserCode());
+        fileInfo.setUnitCode(dataOptContext.getCurrentUnitCode());
+        fileInfo.setContent(TikaTextExtractor.extractInputStreamText(DataSetOptUtil.getInputStreamFormFile(dataSet.getFirstRow())));
+        fileInfo.setFileId(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(bizOptJson.getString("documentId"))));
+        fileInfo.setFileName(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(bizOptJson.getString("fileName"))));
+        fileInfo.setFileSummary(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(bizOptJson.getString("fileSummary"))));
+        fileInfo.setFileMD5(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(bizOptJson.getString("fileMD5"))));
+        fileInfo.setOptTag(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(bizOptJson.getString("optTag"))));
+        fileInfo.setCreateTime(new Date());
+        Object keywords = modelTrasform.attainExpressionValue(bizOptJson.getString("keywords"));
+        if(keywords != null) {
+            fileInfo.setKeywords(StringBaseOpt.castObjectToString(keywords).split(" "));
+        }
+        return  fileInfo;
+    }
 
-            fileInfo.setOptMethod("index");
-            fileInfo.setUserCode(dataOptContext.getCurrentUserCode());
-            fileInfo.setUnitCode(dataOptContext.getCurrentUnitCode());
-            fileInfo.setContent(TikaTextExtractor.extractInputStreamText(
-                DataSetOptUtil.getInputStreamFormFile(dataSet.getFirstRow())));
+    //es 对象文档操作
+    private ResponseData objectDocumentOperation(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext,
+                                                 ESIndexer esIndexer,String operationType){
+        String dataSetId = bizOptJson.getString("source");
+        if (StringUtils.isBlank(dataSetId)) return ResponseData.makeErrorMessage("参数来源不能为空！");
 
-            BizModelJSONTransform modelTrasform = new BizModelJSONTransform(bizModel);
-            String fileId = bizOptJson.getString("fileId");
-            if (StringUtils.isBlank(fileId)){
-                return ResponseData.makeErrorMessage("文档主键不能为空！");
-            }
+        DataSet dataSet = bizModel.getDataSet(dataSetId);
+        if (dataSet == null ) return ResponseData.makeErrorMessage("选择的参数来源不存在！");
 
-            fileInfo.setFileId(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(fileId)));
+        String optTag = bizOptJson.getString("optTag");
+        if (StringUtils.isBlank(optTag)) return ResponseData.makeErrorMessage("业务主键不能为空！");
 
-            String fileName = bizOptJson.getString("fileName");
-            if(StringUtils.isNotBlank(fileName)) {
-                fileInfo.setFileName(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(fileName)));
-            }
-
-            String fileSummary = bizOptJson.getString("fileSummary");
-            if(StringUtils.isNotBlank(fileSummary)) {
-                fileInfo.setFileSummary(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(fileSummary)));
-            }
-
-            String fileMD5 = bizOptJson.getString("fileMD5");
-            if(StringUtils.isNotBlank(fileMD5)) {
-                fileInfo.setFileMD5(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(fileMD5)));
-            }
-
-            String keywords = bizOptJson.getString("keywords");
-            if(StringUtils.isNotBlank(keywords)) {
-                fileInfo.setKeywords(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(keywords)).split(" "));
-            }
-
-            if(StringUtils.isNotBlank(optTag)) {
-                fileInfo.setOptTag(StringBaseOpt.castObjectToString(modelTrasform.attainExpressionValue(optTag)));
-            }
-            fileInfo.setCreateTime(new Date());
-            esIndexer.mergeDocument(fileInfo);
-        } else {
-            List<Map<String, Object>> dataAsList = dataSet.getDataAsList();
+        if("delete".equals(operationType)){
+            Object documentId = JSONTransformer.transformer(
+                BuiltInOperation.getJsonFieldString(bizOptJson, "documentId", null), new BizModelJSONTransform(bizModel));
+            if (documentId == null ) return ResponseData.makeErrorMessage("文档主键不能为空！");
+            boolean b = esIndexer.deleteDocument(StringBaseOpt.castObjectToString(documentId));
+            return ResponseData.makeResponseData(b);
+        }else {
             VariableFormula formula = new VariableFormula();
+            List<Map<String, Object>> dataAsList = dataSet.getDataAsList();
             int rowInd = 0;
             int rowCount = dataAsList.size();
-            for(Map<String, Object> documentInfo : dataAsList){
-                formula.setTrans(new DataRowVariableTranslate(documentInfo, rowInd ++, rowCount));
-                ObjectDocument objInfo = new ObjectDocument();
-                objInfo.setOptId(dataOptContext.getOptId());
-                objInfo.setOsId(dataOptContext.getOsId());
-
-                objInfo.setOptMethod("index");
-                objInfo.setUserCode(dataOptContext.getCurrentUserCode());
-                objInfo.setUnitCode(dataOptContext.getCurrentUnitCode());
-                objInfo.setOptTag(StringBaseOpt.castObjectToString(formula.calcFormula(optTag)));
-                objInfo.contentObject(documentInfo);
-                String title = bizOptJson.getString("title");
-                if (StringUtils.isNotBlank(title)){
-                    objInfo.setTitle(StringBaseOpt.castObjectToString(formula.calcFormula(title)));
+            List<Object> addResult = new ArrayList<>();
+            for(Map<String, Object> documentInfo : dataAsList) {
+                formula.setTrans(new DataRowVariableTranslate(documentInfo, rowInd++, rowCount));
+                switch (operationType) {
+                    case "add":
+                        addResult.add(esIndexer.saveNewDocument(objectDocumentBuild(formula, documentInfo, bizOptJson, dataOptContext)));
+                        break;
+                    case "update":
+                        addResult.add(esIndexer.updateDocument(objectDocumentBuild(formula, documentInfo, bizOptJson, dataOptContext)));
+                        break;
+                    default: //合并
+                        addResult.add(esIndexer.mergeDocument(objectDocumentBuild(formula, documentInfo, bizOptJson, dataOptContext)));
                 }
-                objInfo.setCreateTime(new Date());
-                esIndexer.mergeDocument(objInfo);
             }
+            return  ResponseData.makeResponseData(addResult);
         }
-        return BuiltInOperation.createResponseSuccessData(dataSet.getSize());
+    }
+    // 构建对象文档信息
+    private ObjectDocument objectDocumentBuild(VariableFormula formula,Map<String, Object> documentInfo,JSONObject bizOptJson,DataOptContext dataOptContext){
+        ObjectDocument objInfo = new ObjectDocument();
+        objInfo.setOptId(dataOptContext.getOptId());
+        objInfo.setOsId(dataOptContext.getOsId());
+        objInfo.setOptMethod("index");
+        objInfo.setUserCode(dataOptContext.getCurrentUserCode());
+        objInfo.setUnitCode(dataOptContext.getCurrentUnitCode());
+        objInfo.setOptTag(StringBaseOpt.castObjectToString(formula.calcFormula(bizOptJson.getString("optTag"))));
+        objInfo.contentObject(documentInfo);
+        Object title = formula.calcFormula(bizOptJson.getString("title"));
+        objInfo.setTitle(StringBaseOpt.castObjectToString(title));
+        objInfo.setCreateTime(new Date());
+        return objInfo;
     }
 }
