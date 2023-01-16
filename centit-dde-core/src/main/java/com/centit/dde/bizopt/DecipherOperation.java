f5e77cdefb94ecb9b65e7362fe48b815e8e498cd
codefan <codefan@centit.com>
2023-01-16 11:08:34 +0800

    重构代码

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/DecipherOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/DecipherOperation.java
@@ -6,6 +6,7 @@ import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
+import com.centit.dde.utils.BizModelJSONTransform;
 import com.centit.framework.common.ResponseData;
 import com.centit.framework.security.utils.SM4Util;
 import com.centit.support.algorithm.BooleanBaseOpt;
@@ -16,6 +17,8 @@ import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 public class DecipherOperation implements BizOperation {
 
@@ -28,6 +31,8 @@ public class DecipherOperation implements BizOperation {
         //AES / SM4
         String algorithm = BuiltInOperation.getJsonFieldString(bizOptJson, "algorithm", "AES");
         String password = BuiltInOperation.getJsonFieldString(bizOptJson, "password", "change it");
+        BizModelJSONTransform transform = new BizModelJSONTransform(bizModel);
+        password = StringBaseOpt.castObjectToString(transform.attainExpressionValue(password), password);
         Boolean base64 =
             BooleanBaseOpt.castObjectToBoolean(
                 BuiltInOperation.getJsonFieldString(bizOptJson, "base64", "true"), true);
@@ -37,33 +42,74 @@ public class DecipherOperation implements BizOperation {
             return BuiltInOperation.createResponseData(0, 1,
                 ResponseData.ERROR_OPERATION, "解密算法异常，请指定数据集！");
         }
-        String cipherFieldName = BuiltInOperation.getJsonFieldString(bizOptJson, "cipherField", sourDsName);
 
-        Object cipherData;
-        if(StringUtils.isNotBlank(cipherFieldName)){
-            cipherData = dataSet.getFirstRow().get(cipherFieldName);
+        String cipherDataType = BuiltInOperation.getJsonFieldString(bizOptJson, "fieldName", "");
+        String fieldName = BuiltInOperation.getJsonFieldString(bizOptJson, "fieldName", "");
+
+        /**
+         * 解密数据 cipherData ，整个数据集 dataSet， 某一个具体的字段 field ： fieldName ， cipherFieldName
+         */
+        if("dataSet".equals(cipherDataType)) {
+            Object cipherData;
+            if (StringUtils.isNotBlank(fieldName)) {
+                cipherData = dataSet.getFirstRow().get(fieldName);
+            } else {
+                cipherData = dataSet.getData();
+            }
+            if (cipherData == null) {
+                return BuiltInOperation.createResponseData(0, 1,
+                    ResponseData.ERROR_OPERATION, "解密算法异常，指定数据内容为null！");
+            }
+            byte[] cipherBytes;
+            if (base64) {
+                String cipherText = StringBaseOpt.castObjectToString(cipherData);
+                cipherBytes = Base64.decodeBase64(cipherText);
+            } else {
+                cipherBytes = ByteBaseOpt.castObjectToBytes(cipherData);
+            }
+            byte[] objectText = "SM4".equalsIgnoreCase(algorithm) ?
+                //这个国密算法有很多策略，现在也搞不懂，随便搞一个
+                SM4Util.decryptEcbPadding(password.getBytes(StandardCharsets.UTF_8), cipherBytes) :
+                AESSecurityUtils.decrypt(cipherBytes, password);
+            //整个数据集都是以json形式存储的
+            String jsonString = new String(objectText);
+            DataSet objectToDataSet = new DataSet(JSON.parse(jsonString));
+            bizModel.putDataSet(targetDsName, objectToDataSet);
+            return BuiltInOperation.createResponseSuccessData(objectToDataSet.getSize());
         } else {
-            cipherData = dataSet.getData();
-        }
-        if (cipherData == null){
-            return BuiltInOperation.createResponseData(0, 1,
-                ResponseData.ERROR_OPERATION, "解密算法异常，指定数据内容为null！");
-        }
-        byte[] cipherBytes;
-        if(base64){
-            String cipherText = StringBaseOpt.castObjectToString(cipherData);
-            cipherBytes = Base64.decodeBase64(cipherText);
-        }else {
-            cipherBytes = ByteBaseOpt.castObjectToBytes(cipherData);
-        }
-        byte[] objectText = "SM4".equalsIgnoreCase(algorithm)?
-            //这个国密算法有很多策略，现在也搞不懂，随便搞一个
-            SM4Util.decryptEcbPadding(password.getBytes(StandardCharsets.UTF_8), cipherBytes):
-            AESSecurityUtils.decrypt(cipherBytes, password);
+            String cipherField = BuiltInOperation.getJsonFieldString(bizOptJson, "cipherFieldName", "");
+            if (StringUtils.isBlank(fieldName) || StringUtils.isBlank(cipherField)) {
+                return BuiltInOperation.createResponseData(0, 1,
+                    ResponseData.ERROR_OPERATION, "加密计算异常，请指定明文字段和者密文字段！");
+            }
 
-        String jsonString = new String(objectText);
-        DataSet objectToDataSet = new DataSet(JSON.parse(jsonString));
-        bizModel.putDataSet(targetDsName, objectToDataSet);
-        return BuiltInOperation.createResponseSuccessData(objectToDataSet.getSize());
+            List<Map<String, Object>> encryptData = dataSet.getDataAsList();
+            if(encryptData.size()<1){
+                return BuiltInOperation.createResponseSuccessData(0);
+            }
+
+            for(Map<String, Object> map : encryptData){
+                Object mw = map.get(fieldName);
+                if(mw!=null){
+                    byte[] cipherBytes;
+                    if (base64) {
+                        String cipherText = StringBaseOpt.castObjectToString(mw);
+                        cipherBytes = Base64.decodeBase64(cipherText);
+                    } else {
+                        cipherBytes = ByteBaseOpt.castObjectToBytes(mw);
+                    }
+
+                    byte[] objectText = "SM4".equalsIgnoreCase(algorithm) ?
+                        //这个国密算法有很多策略，现在也搞不懂，随便搞一个
+                        SM4Util.decryptEcbPadding(password.getBytes(StandardCharsets.UTF_8), cipherBytes) :
+                        AESSecurityUtils.decrypt(cipherBytes, password);
+
+                    map.put(cipherField, JSON.parse(StringBaseOpt.castObjectToString(objectText)));// objectText);
+                }
+            }
+            DataSet objectToDataSet = dataSet.getSize() == 1? new DataSet(encryptData.get(0)) : new DataSet(encryptData);
+            bizModel.putDataSet(targetDsName, objectToDataSet);
+            return BuiltInOperation.createResponseSuccessData(dataSet.getSize());
+        }
     }
 }
--- centit-dde-core/src/main/java/com/centit/dde/bizopt/EncryptOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/EncryptOperation.java
@@ -1,17 +1,25 @@
 package com.centit.dde.bizopt;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
+import com.centit.dde.utils.BizModelJSONTransform;
 import com.centit.framework.common.ResponseData;
 import com.centit.framework.security.utils.SM4Util;
 import com.centit.support.algorithm.BooleanBaseOpt;
+import com.centit.support.algorithm.ByteBaseOpt;
+import com.centit.support.algorithm.StringBaseOpt;
 import com.centit.support.security.AESSecurityUtils;
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+
 
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author codefan@sina.com
@@ -26,25 +34,62 @@ public class EncryptOperation  implements BizOperation {
         //AES / SM4
         String algorithm = BuiltInOperation.getJsonFieldString(bizOptJson, "algorithm", "AES");
         String password = BuiltInOperation.getJsonFieldString(bizOptJson, "password", "change it");
+        BizModelJSONTransform transform = new BizModelJSONTransform(bizModel);
+        password = StringBaseOpt.castObjectToString(transform.attainExpressionValue(password), password);
         Boolean base64 =
             BooleanBaseOpt.castObjectToBoolean(
                 BuiltInOperation.getJsonFieldString(bizOptJson, "base64", "true"), true);
 
         DataSet dataSet = bizModel.getDataSet(sourDsName);
-        if (dataSet==null){
+        if (dataSet == null) {
             return BuiltInOperation.createResponseData(0, 1,
                 ResponseData.ERROR_OPERATION, "加密计算异常，请指定数据集！");
         }
+        /**
+         * 加密数据 encryptData，整个数据集 dataSet， 某一个具体的字段 field ： 密文字段 fieldName ，解密后字段 encryptFieldName
+         */
+        String encryptDataType = BuiltInOperation.getJsonFieldString(bizOptJson, "encryptData", "dataSet");
+        if("dataSet".equals(encryptDataType)) {
+            byte[] cipherText = "SM4".equalsIgnoreCase(algorithm) ?
+                //这个国密算法有很多策略，现在也搞不懂，随便搞一个
+                SM4Util.encryptEcbPadding(password.getBytes(StandardCharsets.UTF_8),
+                    dataSet.toJSONString().getBytes(StandardCharsets.UTF_8)) :
+                AESSecurityUtils.encrypt(dataSet.toJSONString().getBytes(StandardCharsets.UTF_8),
+                    password);
 
-        byte[] cipherText = "SM4".equalsIgnoreCase(algorithm)?
-            //这个国密算法有很多策略，现在也搞不懂，随便搞一个
-            SM4Util.encryptEcbPadding(password.getBytes(StandardCharsets.UTF_8),
-                dataSet.toJSONString().getBytes(StandardCharsets.UTF_8)):
-            AESSecurityUtils.encrypt(dataSet.toJSONString().getBytes(StandardCharsets.UTF_8),
-                password);
+            DataSet objectToDataSet = new DataSet(base64 ? new String(Base64.encodeBase64(cipherText)) : cipherText);
+            bizModel.putDataSet(targetDsName, objectToDataSet);
+            return BuiltInOperation.createResponseSuccessData(objectToDataSet.getSize());
+        } else {
+            String fieldName = BuiltInOperation.getJsonFieldString(bizOptJson, "fieldName", "");
+            String encryptFieldName = BuiltInOperation.getJsonFieldString(bizOptJson, "encryptFieldName", "");
+            if (StringUtils.isBlank(fieldName) || StringUtils.isBlank(encryptFieldName)) {
+                return BuiltInOperation.createResponseData(0, 1,
+                    ResponseData.ERROR_OPERATION, "加密计算异常，请指定明文字段和者密文字段！");
+            }
+            List<Map<String, Object>> encryptData = dataSet.getDataAsList();
+            if(encryptData.size()<1){
+                return BuiltInOperation.createResponseSuccessData(0);
+            }
 
-        DataSet objectToDataSet = new DataSet(base64? new String(Base64.encodeBase64(cipherText)): cipherText );
-        bizModel.putDataSet(targetDsName, objectToDataSet);
-        return BuiltInOperation.createResponseSuccessData(objectToDataSet.getSize());
+            for(Map<String, Object> map : encryptData){
+                Object mw = map.get(fieldName);
+                if(mw!=null){
+                    byte[] cipherText = "SM4".equalsIgnoreCase(algorithm) ?
+                        //这个国密算法有很多策略，现在也搞不懂，随便搞一个
+                        SM4Util.encryptEcbPadding(password.getBytes(StandardCharsets.UTF_8),
+                            JSON.toJSONString(mw).getBytes(StandardCharsets.UTF_8)) ://
+                            //ByteBaseOpt.castObjectToBytes(mw)) :
+                        AESSecurityUtils.encrypt(JSON.toJSONString(mw).getBytes(StandardCharsets.UTF_8), password);
+                    if(base64)
+                        map.put(encryptFieldName, new String(Base64.encodeBase64(cipherText)));
+                    else
+                        map.put(encryptFieldName, cipherText);
+                }
+            }
+            DataSet objectToDataSet = dataSet.getSize() == 1? new DataSet(encryptData.get(0)) : new DataSet(encryptData);
+            bizModel.putDataSet(targetDsName, objectToDataSet);
+            return BuiltInOperation.createResponseSuccessData(dataSet.getSize());
+        }
     }
 }
