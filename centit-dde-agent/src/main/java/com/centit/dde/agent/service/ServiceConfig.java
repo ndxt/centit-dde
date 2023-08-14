fde1999a287e0d64cc636aaa9b715e22b90963a9
zhf <zhf@centit.com>
2023-08-14 15:10:38 +0800

    添加工作流依赖

--- centit-dde-agent/src/main/java/com/centit/dde/agent/service/ServiceConfig.java
+++ centit-dde-agent/src/main/java/com/centit/dde/agent/service/ServiceConfig.java
@@ -1,10 +1,14 @@
 package com.centit.dde.agent.service;
 
+import com.centit.framework.components.impl.NotificationCenterImpl;
 import com.centit.framework.components.impl.TextOperationLogWriterImpl;
 import com.centit.framework.core.service.DataScopePowerManager;
 import com.centit.framework.core.service.impl.DataScopePowerManagerImpl;
+import com.centit.framework.model.adapter.NotificationCenter;
 import com.centit.framework.model.adapter.OperationLogWriter;
+import com.centit.framework.model.adapter.UserUnitFilterCalcContextFactory;
 import com.centit.framework.security.model.StandardPasswordEncoderImpl;
+import com.centit.workflow.service.impl.SystemUserUnitCalcContextFactoryImpl;
 import org.quartz.Scheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -34,6 +38,16 @@ public class ServiceConfig {
         return schedulerFactoryBean().getScheduler();
     }
 
+    @Bean
+    public NotificationCenter notificationCenter() {
+        NotificationCenterImpl notificationCenter = new NotificationCenterImpl();
+        notificationCenter.initDummyMsgSenders();
+        return notificationCenter;
+    }
+    @Bean
+    public UserUnitFilterCalcContextFactory userUnitFilterFactory() {
+        return new SystemUserUnitCalcContextFactoryImpl();
+    }
     @Bean
     public OperationLogWriter operationLogWriter() {
         TextOperationLogWriterImpl textOperationLogWriter
