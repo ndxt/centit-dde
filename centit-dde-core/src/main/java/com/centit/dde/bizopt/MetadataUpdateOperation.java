7aff8255b29662dcff927c6f92e2efe35f2b937a
codefan <codefan@centit.com>
2023-09-15 13:30:40 +0800

    添加 逻辑删除 和 更新前校验版本的操作

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/MetadataUpdateOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/MetadataUpdateOperation.java
@@ -36,7 +36,7 @@ public class MetadataUpdateOperation implements BizOperation {
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext) throws Exception {
         String id = bizOptJson.getString("id");
         Integer updateType = bizOptJson.getInteger("updateType");
-        Map<String, Object> parames = DataSetOptUtil.getDataSetParames(bizModel, bizOptJson);
+        Map<String, Object> objectMap = DataSetOptUtil.getDataSetParames(bizModel, bizOptJson);
         String tableId = bizOptJson.getString("tableId");
         Integer withChildrenDeep = NumberBaseOpt.castObjectToInteger(bizOptJson.getInteger("withChildrenDeep"),1);
 
@@ -56,30 +56,38 @@ public class MetadataUpdateOperation implements BizOperation {
             if (filters != null && filters.size() > 0) {
                 DataPowerFilter dataPowerFilter = queryDataScopeFilter.createUserDataPowerFilter(currentUserDetails);
 
-                if (!dataPowerFilter.checkObject(parames, filters)) {
+                if (!dataPowerFilter.checkObject(objectMap, filters)) {
                     throw new ObjectException(ObjectException.DATA_VALIDATE_ERROR,
                         "数据范围权限校验不通过，用户：" + currentUserCode + "，校验条件" + JSON.toJSONString(filters) + "。");
                 }
             }
         }
-
+        int updateCount = 1;
         switch (updateType){
             case 1://新建
-                int count = metaObjectService.saveObjectWithChildren(tableId, parames, withChildrenDeep);
-                bizModel.putDataSet(id, new DataSet(parames));
-                return BuiltInOperation.createResponseSuccessData(count);
+                updateCount = metaObjectService.saveObjectWithChildren(tableId, objectMap, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(objectMap));
+                return BuiltInOperation.createResponseSuccessData(updateCount);
             case 2://修改
-                int upcount= metaObjectService.updateObjectWithChildren(tableId, parames, withChildrenDeep);
-                bizModel.putDataSet(id, new DataSet(parames));
-                return BuiltInOperation.createResponseSuccessData(upcount);
+                updateCount = metaObjectService.updateObjectWithChildren(tableId, objectMap, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(objectMap));
+                return BuiltInOperation.createResponseSuccessData(updateCount);
             case 3://删除
-                metaObjectService.deleteObjectWithChildren(tableId, parames, withChildrenDeep);
-                bizModel.putDataSet(id, new DataSet(parames));
+                metaObjectService.deleteObjectWithChildren(tableId, objectMap, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(objectMap));
                 return BuiltInOperation.createResponseSuccessData(1);
             case 4://合并
-                int resultCount = metaObjectService.mergeObjectWithChildren(tableId, parames, withChildrenDeep);
-                bizModel.putDataSet(id, new DataSet(parames));
-                return BuiltInOperation.createResponseSuccessData(resultCount);
+                updateCount = metaObjectService.mergeObjectWithChildren(tableId, objectMap, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(objectMap));
+                return BuiltInOperation.createResponseSuccessData(updateCount);
+            case 5://逻辑删除
+                metaObjectService.softDeleteObjectWithChildren(tableId, objectMap, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(objectMap));
+                return BuiltInOperation.createResponseSuccessData(1);
+            case 6://带版本更新
+                updateCount = metaObjectService.updateObjectWithChildrenCheckVersion(tableId, objectMap, withChildrenDeep);
+                bizModel.putDataSet(id, new DataSet(objectMap));
+                return BuiltInOperation.createResponseSuccessData(updateCount);
             default:
                 return ResponseData.makeErrorMessage("未知操作类型！");
         }
