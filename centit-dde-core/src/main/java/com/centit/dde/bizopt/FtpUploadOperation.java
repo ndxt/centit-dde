1e4d3124b2406085e0bfffd5248189c8eca7c41d
codedfan <codedfan@sina.com>
2024-01-18 16:17:55 +0800

    给ftp添加编码属性

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/FtpUploadOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/FtpUploadOperation.java
@@ -6,10 +6,14 @@ import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
 import com.centit.dde.dataset.FileDataSet;
+import com.centit.dde.utils.BizModelJSONTransform;
 import com.centit.dde.utils.DataSetOptUtil;
 import com.centit.dde.utils.FtpOperation;
 import com.centit.framework.common.ResponseData;
 import com.centit.product.metadata.dao.SourceInfoDao;
+import com.centit.support.algorithm.StringBaseOpt;
+import com.centit.support.json.JSONTransformer;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.net.ftp.FTPClient;
 
 public class FtpUploadOperation extends FtpOperation implements BizOperation {
@@ -24,17 +28,27 @@ public class FtpUploadOperation extends FtpOperation implements BizOperation {
 
         String sourDsName = BuiltInOperation.getJsonFieldString(bizOptJson, "source", bizModel.getModelName());
         DataSet dataSet = bizModel.getDataSet(sourDsName);
-        String path = BuiltInOperation.getJsonFieldString(bizOptJson, "filePath", "/");
+        String filePathDesc = BuiltInOperation.getJsonFieldString(bizOptJson, "filePath", "");
+
+        BizModelJSONTransform transformer = new BizModelJSONTransform(bizModel);
+        String filePath = StringBaseOpt.objectToString(JSONTransformer.transformer(filePathDesc, transformer));
+        if(StringUtils.isBlank(filePath)){
+            if(StringUtils.isBlank(filePathDesc)){
+                filePath = "/";
+            } else {
+                filePath = filePathDesc;
+            }
+        }
 
         FileDataSet fileInfo = DataSetOptUtil.attainFileDataset(dataSet, bizOptJson);
 
         FTPClient ftpClient = connectFtp(ftpServiceId);
         if(ftpClient==null){
-            return BuiltInOperation.createResponseData(0,1,ResponseData.ERROR_USER_CONFIG,
+            return BuiltInOperation.createResponseData(0, 1, ResponseData.ERROR_USER_CONFIG,
                 "FPT服务配置信息错误！");
         }
         try{
-            ftpClient.changeWorkingDirectory(path);
+            ftpClient.changeWorkingDirectory(filePath);
             ftpClient.storeFile(fileInfo.getFileName(),
                 fileInfo.getFileInputStream());
         } finally {
