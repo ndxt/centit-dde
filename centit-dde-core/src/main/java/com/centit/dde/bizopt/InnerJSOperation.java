c8d09808b11a4e2f77a8aee538874f6dc1f7871c
codefan <codefan@centit.com>
2024-05-29 15:32:22 +0800

    添加对python的支持，需要前端添加新特性，暂时不实现

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/InnerJSOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/InnerJSOperation.java
@@ -6,7 +6,9 @@ import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
 import com.centit.framework.common.ResponseData;
+import com.centit.support.extend.AbstractRuntimeContext;
 import com.centit.support.extend.JSRuntimeContext;
+import com.centit.support.extend.PythonRuntimeContext;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -15,15 +17,21 @@ import org.apache.commons.lang3.StringUtils;
 public class InnerJSOperation implements BizOperation {
     @Override
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext) throws Exception {
-        JSRuntimeContext jsRuntimeContext = new JSRuntimeContext();
-
         String targetDsName = BuiltInOperation.getJsonFieldString(bizOptJson, "id", "js");
         String javaScript = BuiltInOperation.getJsonFieldString(bizOptJson, "valuejs", "");
+        String scriptType = BuiltInOperation.getJsonFieldString(bizOptJson, "scriptType", "js");
+        AbstractRuntimeContext runtimeContext;
+        if("python".equals(scriptType)){
+            // 需要添加 jython-standalone 依赖
+            runtimeContext = new PythonRuntimeContext();
+        } else {
+            runtimeContext = new JSRuntimeContext();
+        }
         if (StringUtils.isNotBlank(javaScript)) {
-            jsRuntimeContext.compileScript(javaScript);
+            runtimeContext.compileScript(javaScript);
         }
         int count = 0;
-        Object object = jsRuntimeContext.callJsFunc("runOpt",bizModel);
+        Object object = runtimeContext.callJsFunc("runOpt", bizModel);
         bizModel.putDataSet(targetDsName, DataSet.toDataSet(object));
         if (object != null) {
             count = bizModel.getDataSet(targetDsName).getSize();
