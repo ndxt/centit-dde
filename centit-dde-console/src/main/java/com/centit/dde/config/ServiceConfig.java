71c7fc957a96fda10e4ad94f75db56b296f4d631
codefan <codefan@centit.com>
2024-06-27 11:34:24 +0800

    整理代码，填补缺失的头文件

--- centit-dde-console/src/main/java/com/centit/dde/config/ServiceConfig.java
+++ centit-dde-console/src/main/java/com/centit/dde/config/ServiceConfig.java
@@ -27,6 +27,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
