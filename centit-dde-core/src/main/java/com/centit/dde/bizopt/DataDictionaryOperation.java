06efa33dca14013c60a3a08f9a0859642bce9f27
zhf <zhf@centit.com>
2025-04-17 15:27:33 +0800

    优化数据字典

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/DataDictionaryOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/DataDictionaryOperation.java
@@ -5,13 +5,16 @@ import com.centit.dde.core.BizModel;
 import com.centit.dde.core.BizOperation;
 import com.centit.dde.core.DataOptContext;
 import com.centit.dde.core.DataSet;
+import com.centit.dde.utils.BizModelJSONTransform;
 import com.centit.framework.common.ResponseData;
 import com.centit.framework.components.CodeRepositoryUtil;
 import com.centit.framework.model.basedata.DataDictionary;
 import com.centit.support.algorithm.BooleanBaseOpt;
 import com.centit.support.algorithm.CollectionsOpt;
 import com.centit.support.algorithm.NumberBaseOpt;
+import com.centit.support.algorithm.StringBaseOpt;
 import com.centit.support.common.TreeNode;
+import com.centit.support.json.JSONTransformer;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.List;
@@ -22,17 +25,20 @@ import java.util.List;
 public class DataDictionaryOperation implements BizOperation {
     @Override
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext){
-
+        BizModelJSONTransform bizModelJSONTransform = new BizModelJSONTransform(bizModel);
         String catalog = bizOptJson.getString("catalog");
+        catalog=StringBaseOpt.castObjectToString(JSONTransformer.transformer(catalog, bizModelJSONTransform));
         boolean asTree = BooleanBaseOpt.castObjectToBoolean(bizOptJson.get("asTree"), false);
         String startCode = bizOptJson.getString("startCode");
+        startCode=StringBaseOpt.castObjectToString(JSONTransformer.transformer(startCode, bizModelJSONTransform));
         int levels = NumberBaseOpt.castObjectToInteger(bizOptJson.get("levels"), -1);
 
         List<DataDictionary> dicts = CodeRepositoryUtil.getDictionary(catalog);
         if(asTree) {
             if(StringUtils.isNotBlank(startCode)){
+                String finalStartCode = startCode;
                 TreeNode<DataDictionary> treeNode =
-                    CollectionsOpt.fetchTreeBranch( dicts, (b) -> StringUtils.equals(startCode , b.getDataCode()) ,
+                    CollectionsOpt.fetchTreeBranch( dicts, (b) -> StringUtils.equals(finalStartCode, b.getDataCode()) ,
                     (p, c) -> StringUtils.equals(p.getDataCode(), c.getExtraCode()), levels);
                 bizModel.putDataSet(bizOptJson.getString("id"), new DataSet(treeNode.toJSONObject()));
             } else {
