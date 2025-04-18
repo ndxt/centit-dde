922b9790d967141dd25558a9ffff6f7dd757b2f1
codefan <codefan@centit.com>
2025-04-18 10:05:05 +0800

    fix NPE

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/DataDictionaryOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/DataDictionaryOperation.java
@@ -13,6 +13,7 @@ import com.centit.support.algorithm.BooleanBaseOpt;
 import com.centit.support.algorithm.CollectionsOpt;
 import com.centit.support.algorithm.NumberBaseOpt;
 import com.centit.support.algorithm.StringBaseOpt;
+import com.centit.support.common.ObjectException;
 import com.centit.support.common.TreeNode;
 import com.centit.support.json.JSONTransformer;
 import org.apache.commons.lang3.StringUtils;
@@ -27,13 +28,17 @@ public class DataDictionaryOperation implements BizOperation {
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext){
         BizModelJSONTransform bizModelJSONTransform = new BizModelJSONTransform(bizModel);
         String catalog = bizOptJson.getString("catalog");
-        catalog=StringBaseOpt.castObjectToString(JSONTransformer.transformer(catalog, bizModelJSONTransform));
+        catalog = StringBaseOpt.castObjectToString(JSONTransformer.transformer(catalog, bizModelJSONTransform), catalog);
         boolean asTree = BooleanBaseOpt.castObjectToBoolean(bizOptJson.get("asTree"), false);
         String startCode = bizOptJson.getString("startCode");
-        startCode=StringBaseOpt.castObjectToString(JSONTransformer.transformer(startCode, bizModelJSONTransform));
+        startCode = StringBaseOpt.castObjectToString(JSONTransformer.transformer(startCode, bizModelJSONTransform), startCode);
         int levels = NumberBaseOpt.castObjectToInteger(bizOptJson.get("levels"), -1);
 
         List<DataDictionary> dicts = CodeRepositoryUtil.getDictionary(catalog);
+        if(dicts == null || dicts.isEmpty()){
+            return BuiltInOperation.createResponseData(0,1,
+                ObjectException.DATA_NOT_FOUND_EXCEPTION, "没有找到对应的数据字典："+ catalog);
+        }
         if(asTree) {
             if(StringUtils.isNotBlank(startCode)){
                 String finalStartCode = startCode;
