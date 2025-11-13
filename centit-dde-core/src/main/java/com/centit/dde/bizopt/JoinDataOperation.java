0f61f854842ce833898633ac0fb91ce29feae5f0
codefan <codefan@centit.com>
2025-11-13 15:01:41 +0800

    整理代码

--- centit-dde-core/src/main/java/com/centit/dde/bizopt/JoinDataOperation.java
+++ centit-dde-core/src/main/java/com/centit/dde/bizopt/JoinDataOperation.java
@@ -17,24 +17,24 @@ public class JoinDataOperation implements BizOperation {
     @Override
     public ResponseData runOpt(BizModel bizModel, JSONObject bizOptJson, DataOptContext dataOptContext) throws Exception {
 
-            String sour1DsName = BuiltInOperation.getJsonFieldString(bizOptJson, "source1", null);
-            String sour2DsName = BuiltInOperation.getJsonFieldString(bizOptJson, "source2", null);
-            String join = BuiltInOperation.getJsonFieldString(bizOptJson, "operation", "join");
-            Map<String, String> map = BuiltInOperation.jsonArrayToMap(bizOptJson.getJSONArray("configfield"), "primaryKey1", "primaryKey2");
-            if (map != null) {
-                DataSet dataSet = bizModel.getDataSet(sour1DsName);
-                DataSet dataSet2 = bizModel.getDataSet(sour2DsName);
-                DataSet destDs;
-                if(ConstantValue.DATASET_JOIN_TYPE_APPEND.equalsIgnoreCase(join)){
-                    destDs = DataSetOptUtil.leftAppendTwoDataSet(dataSet, dataSet2, new ArrayList<>(map.entrySet()));
-                } else {
-                    destDs = DataSetOptUtil.joinTwoDataSet(dataSet, dataSet2, new ArrayList<>(map.entrySet()), join);
-                }
-                if (destDs != null) {
-                    bizModel.putDataSet(BuiltInOperation.getJsonFieldString(bizOptJson, "id", bizModel.getModelName()), destDs);
-                    return BuiltInOperation.createResponseSuccessData(destDs.getSize());
-                }
+        String sour1DsName = BuiltInOperation.getJsonFieldString(bizOptJson, "source1", null);
+        String sour2DsName = BuiltInOperation.getJsonFieldString(bizOptJson, "source2", null);
+        String join = BuiltInOperation.getJsonFieldString(bizOptJson, "operation", "join");
+        Map<String, String> map = BuiltInOperation.jsonArrayToMap(bizOptJson.getJSONArray("configfield"), "primaryKey1", "primaryKey2");
+        if (map != null) {
+            DataSet dataSet = bizModel.getDataSet(sour1DsName);
+            DataSet dataSet2 = bizModel.getDataSet(sour2DsName);
+            DataSet destDs;
+            if(ConstantValue.DATASET_JOIN_TYPE_APPEND.equalsIgnoreCase(join)){
+                destDs = DataSetOptUtil.leftAppendTwoDataSet(dataSet, dataSet2, new ArrayList<>(map.entrySet()));
+            } else {
+                destDs = DataSetOptUtil.joinTwoDataSet(dataSet, dataSet2, new ArrayList<>(map.entrySet()), join);
             }
-            return BuiltInOperation.createResponseSuccessData(0);
+            if (destDs != null) {
+                bizModel.putDataSet(BuiltInOperation.getJsonFieldString(bizOptJson, "id", bizModel.getModelName()), destDs);
+                return BuiltInOperation.createResponseSuccessData(destDs.getSize());
+            }
+        }
+        return BuiltInOperation.createResponseSuccessData(0);
     }
 }
